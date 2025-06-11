a5969a4473c9412afd0f707923f0df8a6955c6b7
leoacferraz <leoalvo89@gmail.com>
2025-06-10 21:29:55 -0300

    Configurando a busca de usuários

---
+++ src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -0,0 +1,20 @@
+package med.voll.web_application.domain.usuario;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UsuarioService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmailIgnoreCase(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não foi encontrado"));
+    }
+}
