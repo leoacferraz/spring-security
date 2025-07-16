4d23659d271a5a32086758b7c961ed0625bcf8bf
leoacferraz <leoalvo89@gmail.com>
2025-07-16 17:38:20 -0300

    Criando um identificador para o usuário

--- 1-2/3866-seguranca-java-2/src/main/java/med/voll/web_application/domain/usuario/Usuario.java
+++ 1-2/3866-seguranca-java-2/src/main/java/med/voll/web_application/domain/usuario/Usuario.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -25,6 +26,10 @@ public class Usuario implements UserDetails {
     private String email;
     private String senha;
     private Boolean senhaAlterada;
+    private String token;
+    private LocalDateTime expiracaoToken;
+
+
     @Enumerated(EnumType.STRING)
     private Perfil perfil;
 
@@ -75,5 +80,21 @@ public class Usuario implements UserDetails {
 
     public void setSenhaAlterada(Boolean senhaAlterada){
         this.senhaAlterada = senhaAlterada;
+    }
+
+    public LocalDateTime getExpiracaoToken() {
+        return expiracaoToken;
+    }
+
+    public void setExpiracaoToken(LocalDateTime expiracaoToken) {
+        this.expiracaoToken = expiracaoToken;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
